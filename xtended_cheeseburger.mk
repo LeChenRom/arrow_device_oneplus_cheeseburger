8c8fe00f0faa0719873cfb633826c56cc49f419b
LeChenRom <aiwuzuili24@gmail.com>
2018-10-09 19:08:04 +0800

    xtended build

--- xtended_cheeseburger.mk
+++ xtended_cheeseburger.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common xtended stuff.
+$(call inherit-product, vendor/xtended/config/common.mk)
 
-PRODUCT_NAME := arrow_cheeseburger
+PRODUCT_NAME := xtended_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
